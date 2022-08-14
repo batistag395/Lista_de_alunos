463b1e0c6f685814adae9d6bbe951a7ae4689a97
Gustavo Batista <batistag395@gmail.com>
2022-08-14 15:50:17 -0300

    Atualizando parte de logica de exclusao de nomes

--- Lista_de_alunos/Program.cs
+++ Lista_de_alunos/Program.cs
@@ -1,4 +1,5 @@
 ﻿List<string> nomes = new List<string>();
+
 int count = 1;
 
 string nomeParaAdd = " ";
@@ -11,20 +12,23 @@ while (nomeParaAdd.Length != null)
     nomeParaAdd = Console.ReadLine();
     if (nomeParaAdd.Length == 0)
     {
-        break; ;
+        break; 
     }
     else
     {
         nomes.Add(nomeParaAdd);
     }
 }
+
 Console.WriteLine($"Voce cadastrou {nomes.Count} nomes");
 Console.WriteLine("Lista de nomes:");
+
 foreach (string nome in nomes)
 {
     Console.WriteLine("#" + count + " " + nome);
     count++;
 }
+
 Console.WriteLine("Deseja remover alguem da lista? (s/n)");
 char escolha = char.Parse(Console.ReadLine());
 
@@ -45,10 +49,21 @@ while (escolha == 's' || escolha == 'S')
         {
             Console.WriteLine("O nome nao consta na lista, verifique se digitou corretamente!");
             nomeParaAdd = Console.ReadLine();
+
             if (nomes.Contains(nomeParaAdd))
             {
                 Console.WriteLine("Removido com sucesso");
                 nomes.Remove(nomeParaAdd);
+
+                count = 1;
+                Console.WriteLine($"Voce cadastrou {nomes.Count} nomes");
+                Console.WriteLine("Lista de nomes:");
+
+                foreach (string nome in nomes)
+                {
+                    Console.WriteLine("#" + count + " " + nome);
+                    count++;
+                }
                 break;
             }
         }
@@ -58,15 +73,6 @@ while (escolha == 's' || escolha == 'S')
             break;
         }
     }
-
     Console.WriteLine("Deseja remover mais alguem da lista? (s/n)");
     escolha = char.Parse(Console.ReadLine());
-}
-count = 1;
-Console.WriteLine($"Voce cadastrou {nomes.Count} nomes");
-Console.WriteLine("Lista de nomes:");
-foreach (string nome in nomes)
-{
-    Console.WriteLine("#" + count + " " + nome);
-    count++;
 }
